0f1aeb620357e93b2964c1e87b2588e80a3047f1
Nico Jansson <janssnic@arcada.fi>
2025-09-16 14:41:52 +0300

    mlagents learning

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -1,64 +1,118 @@
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-public class Movement : MonoBehaviour
+public class Movement : Agent
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public GameObject scrap;
+    public GameObject Dust;
     public float moveSpeed = 3f;
     float rotationSpeed = 90f;
+    Vector3 StartPosition;
+    float prevDistance = 0;
+
+    public override void OnEpisodeBegin()
+    {
+        Reset();
+    }
+
+    public override void OnActionReceived(ActionBuffers actions)
+    {
+        float currentDistance = Vector3.Distance(transform.localPosition, Dust.transform.localPosition);
+        float Delta = prevDistance - currentDistance;
+
+        if (prevDistance != 0)
+        {
+            AddReward(Delta * 0.01f);    
+        }
+        prevDistance = currentDistance;
+        
+
+        AddReward(-0.001f);
+        //En Array med disreta actions. Två element med värde 0, 1, eller 2
+        //Matchar med de actions man definierat i Beahiour Parameters skriptet
+        var action = actions.DiscreteActions;
+
+        if (action[0] == 1)
+            MoveForward();
+        if (action[0] == 2)
+            MoveBackwards();
+        if (action[1] == 1)
+            TurnRight();
+        if (action[1] == 2)
+            TurnLeft();
+    }
     void Start()
     {
-        PlaceGameObjectAtRandomPosition(scrap);
+        SpawnDust();
+        StartPosition = transform.position;
+        Reset();
     }
 
     // Update is called once per frame
-    void Update()
+    // void Update()
+    // {
+
+    //     if (Keyboard.current.wKey.isPressed)
+    //         MoveForward();
+    //     else if (Keyboard.current.sKey.isPressed)
+    //         MoveBackwards();
+    //     if (Keyboard.current.aKey.isPressed)
+    //         TurnLeft();
+    //     else if (Keyboard.current.dKey.isPressed)
+    //         TurnRight();
+
+    //     if (Keyboard.current.oKey.isPressed)
+    //     {
+    //         SpawnDust();
+    //     }
+    // }
+
+    void Reset()
     {
-
-        float move = 0f;
-        float rotate = 0f;
-
-        if (Keyboard.current.wKey.isPressed)
-            move = 1f;
-        else if (Keyboard.current.sKey.isPressed)
-            move = -1f;
-
-        if (Keyboard.current.aKey.isPressed)
-            rotate = -1f;
-        else if (Keyboard.current.dKey.isPressed)
-            rotate = 1f;
-
-        transform.Translate(Vector3.forward * move * moveSpeed * Time.deltaTime);
-        transform.Rotate(Vector3.up * rotate * rotationSpeed * Time.deltaTime);
-
-        if (Keyboard.current.oKey.isPressed)
-        {
-            PlaceGameObjectAtRandomPosition(scrap);
-        }
+        transform.position = StartPosition;
+        SpawnDust();
     }
 
-    public void PlaceGameObjectAtRandomPosition(GameObject obj, float range = 5f)
+    void MoveForward()
     {
-        if (obj == null) return;
-        Vector3 randomPosition = new Vector3(
-            Random.Range(-range, range),
-            obj.transform.position.y,
-            Random.Range(-range, range)
-        );
-        obj.transform.position = randomPosition;
+        transform.Translate(0, 0, moveSpeed * Time.deltaTime);
     }
+
+    void MoveBackwards()
+    {
+        transform.Translate(0, 0, -moveSpeed * Time.deltaTime);
+    }
+
+    void TurnRight()
+    {
+        transform.Rotate(0, rotationSpeed * Time.deltaTime, 0);
+    }
+
+    void TurnLeft()
+    {
+        transform.Rotate(0, -rotationSpeed * Time.deltaTime, 0);
+    }
+
+    void SpawnDust()
+    {
+        Dust.transform.localPosition = new Vector3(Random.Range(4.0f, -5.0f), Dust.transform.localPosition.y, Random.Range(-12.0f, -3.0f));
+    }
+
     void OnCollisionEnter(Collision collision)
     {
         // Debug.Log(collision.gameObject.tag);
         if (collision.gameObject.tag == "walls")
         {
+            AddReward(-1.0f);
             Debug.Log("fail");
+            EndEpisode();
         }
         else if (collision.gameObject.tag == "scrap")
         {
+            AddReward(1.0f);
             Debug.Log("succes");
-            PlaceGameObjectAtRandomPosition(scrap);
+            EndEpisode();
         }
 
     }
